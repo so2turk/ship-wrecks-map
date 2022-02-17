804f5f5cb21531c8ca29aac9c4fc333a0b28d894
Serhat Ozturk <so2turk@yahoo.com>
2022-02-17 14:40:36 +0100

    Add react-map-gl / mapbox-gl / material-ui and set up map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
+import React, { useState } from 'react'
+import Map, { Marker } from 'react-map-gl'
+import { RoomSharp } from '@material-ui/icons'
 
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 function App() {
+  const [viewState, setViewState] = useState({
+    latitude: 52.520008,
+    longitude: 13.404954,
+    zoom: 6
+  });
+
   return (
-    <div className="App">
-      Ship Wrecks
-    </div>
+    
+      <Map
+        {...viewState}
+        onMove={evt => setViewState(evt.viewState)}
+        style={{width: '100vh', height: '100vh'}}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      >
+        <Marker longitude={13.404954} latitude={52.520008} anchor="bottom" >
+          <RoomSharp />
+        </Marker>
+      </Map>
+    
   );
 }
 
