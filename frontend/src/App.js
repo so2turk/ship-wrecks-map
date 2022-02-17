bc505b0c33f98f56198ffac2811de6e5a6355a2a
Serhat Ozturk <so2turk@yahoo.com>
2022-02-17 17:50:38 +0100

    Fetch data from backend and show with marker/popup card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
-import Map, { Marker } from 'react-map-gl'
+import React, { useEffect, useState } from 'react'
+import Map, { Marker, Popup } from 'react-map-gl'
 import { RoomSharp } from '@material-ui/icons'
-
+import axios from 'axios'
 import 'mapbox-gl/dist/mapbox-gl.css'
+import('./app.css')
 
 function App() {
   const [viewState, setViewState] = useState({
@@ -10,21 +11,62 @@ function App() {
     longitude: 13.404954,
     zoom: 6
   });
+  const [ shipWrecks, setShipWrecks ] = useState([])
+  
+  useEffect(() => {
+    getShipWrecks()
+
+  }, [])
+
+  const getShipWrecks = async () => {
+    try {
+      const res = await axios.get('/ships')
+      setShipWrecks(res.data)
+    } catch(err) {
+      console.log(err)
+    }
+  }
 
   return (
-    
+    <div>
       <Map
         {...viewState}
         onMove={evt => setViewState(evt.viewState)}
-        style={{width: '100vh', height: '100vh'}}
+        style={{width: '100%', height: '100vh'}}
         mapStyle="mapbox://styles/mapbox/streets-v9"
         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
       >
-        <Marker longitude={13.404954} latitude={52.520008} anchor="bottom" >
-          <RoomSharp />
-        </Marker>
+        { shipWrecks.map(shipWreck => (
+          <>
+            <Marker 
+              longitude={shipWreck.londec} 
+              latitude={shipWreck.latdec} 
+              anchor="bottom"
+            >
+              <RoomSharp style={{ color: 'black', fontSize: viewState.zoom * 5 }} />
+            </Marker>
+            <Popup
+              longitude={shipWreck.londec}
+              latitude={shipWreck.londec}
+              anchor="bottom"
+              closeButton={true}
+              closeOnClick={false}
+            >
+              <div className='popupCard'>
+                <label>Fature:</label>
+                <p>{shipWreck.feature_type}</p>
+                <label>Depth:</label>
+                <p>{shipWreck.depth}</p>
+                <label>Chart:</label>
+                <p>{shipWreck.chart}</p>
+                <label>Coordinates:</label>
+                <p>{shipWreck.coordinates}</p>
+              </div>
+            </Popup>
+          </>
+        ))}
       </Map>
-    
+    </div>
   );
 }
 
